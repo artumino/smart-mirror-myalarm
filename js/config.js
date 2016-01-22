9c4fc843d72f47063699d88f08435695ed357421
Jacopo Libé <jacopo.libe@gmail.com>
2016-01-22 02:59:05 +0100

    Cambiato weather system

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-var CONFIG_FILE_LOCATION = "/data/config.json";
-var FORCAST_API_KEY = "8154f7341f0d1f4e67d01846bb36231f";
+var CONFIG_FILE_LOCATION = "data/config.json";
+var FORCAST_API_KEY = "APIKEY";
 var HUE_BASE = "http://{IP_ADDRESS_OF_BRIDGE}/api/{Username}/";
